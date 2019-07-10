ba88dca78d5d3099dd0530b790f3b0c523890f30
MochiTart <52751073+MochiTart@users.noreply.github.com>
2019-07-10 09:37:12 -0500

    Add files via upload

---
+++ Webpage_int.js
@@ -0,0 +1,23 @@
+function imageEnlarge() {
+ document.getElementById('1').style.height="40%";
+ document.getElementById('1').style.width="40%";
+ document.getElementById('1').style.position='static';
+ }
+function imageReset() {
+
+ document.getElementById('1').style.height="20%";
+ document.getElementById('1').style.width="20%"
+ document.getElementById('1').style.position='static';
+ }
+
+ function imageEnlarge2() {
+  document.getElementById('2').style.height="100%";
+  document.getElementById('2').style.width="100%";
+  document.getElementById('2').style.position='static';
+  }
+ function imageReset2() {
+
+  document.getElementById('2').style.height="80%";
+  document.getElementById('2').style.width="80%"
+  document.getElementById('2').style.position='static';
+  }
